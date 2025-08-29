8b95337a54cbda738f64c8b05d7dc1f3a46a82f2
JiyaMehta11 <jiya0704.be23@chitkara.edu.in>
2025-08-29 17:20:40 +0530

    aboutus

--- frontend/src/Pages/AboutUsPage/AboutUs.jsx
+++ frontend/src/Pages/AboutUsPage/AboutUs.jsx
@@ -1,8 +1,37 @@
 import React from 'react'
+import './AboutUs.css'
+import FloatingCards from './FloatingCards'
+
 
 function AboutUs() {
   return (
-    <div></div>
+    <div>
+      <div className="first-container">
+        <div className="first-left">
+         
+          <h2 className="main-heading">Ace Interviews With PrepSphere</h2>
+          <p className="sub-heading">
+            Get role-specific questions, expand answers when you need them, dive deeper into concepts, and organize everything your way.
+          </p>
+        </div>
+
+      <div className="first-right">
+        <form className="search-bar">
+          <span className="search-icon">
+            <svg width="20" height="20" fill="none" stroke="#888" strokeWidth="2" viewBox="0 0 24 24">
+              <circle cx="11" cy="11" r="8" />
+              <line x1="21" y1="21" x2="16.65" y2="16.65" />
+            </svg>
+          </span>
+          <input type="text" placeholder="Search…" />
+        </form>
+      </div>
+      </div>
+      <FloatingCards />
+      <div>
+        
+      </div>
+    </div>
   )
 }
 
